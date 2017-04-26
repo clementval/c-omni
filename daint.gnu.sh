2850026daebee948d9a5f6432376a8459bf67d01
Valentin Clement <clementval@gmail.com>
2017-04-26 13:49:06 +0200

    Add specific libxml2 configuration elements

--- daint.gnu.sh
+++ daint.gnu.sh
@@ -4,4 +4,6 @@ OMNI_CXX="CC"
 
 OMNI_MPI_CC="MPI_CC=cc" 
 OMNI_MPI_FC="MPI_FC=ftn"
-ADDITONAL_OPTIONS="-DOMNI_MPI_CC=$OMNI_MPI_CC -DOMNI_MPI_FC=$OMNI_MPI_FC"
+
+# Fix problem finding libxml2
+OMNI_CONF="--with-libxml2-lib=/usr/lib64/ --with-libxml2-include=/usr/include/"
