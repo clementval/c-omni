31dc0c6639ab7db274ef7c5979dc71125659b957
Valentin Clement <clementval@gmail.com>
2017-04-26 16:50:18 +0200

    Remove specific configure flags

--- daint.gnu.sh
+++ daint.gnu.sh
@@ -3,9 +3,6 @@ OMNI_FC="ftn"
 OMNI_CC="cc"
 OMNI_CXX="CC"
 
-# MPI wrapper used by OMNI Compiler
+# MPI wrapper used by OMNI compiler
 OMNI_MPI_CC="MPI_CC=cc"
 OMNI_MPI_FC="MPI_FC=ftn"
-
-# Fix problem finding libxml2
-OMNI_CONF="--with-libxml2-lib=/usr/lib64/ --with-libxml2-include=/usr/include/"
