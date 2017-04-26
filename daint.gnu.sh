1113c0ac8df0a360cc4e840efedc50446384acc0
Valentin Clement <clementval@gmail.com>
2017-04-26 13:51:18 +0200

    Add comment

--- daint.gnu.sh
+++ daint.gnu.sh
@@ -1,7 +1,9 @@
+# Basic compiler used for OMNI Compiler compilation
 OMNI_FC="ftn"
 OMNI_CC="cc"
 OMNI_CXX="CC"
 
+# MPI wrapper used by OMNI Compiler
 OMNI_MPI_CC="MPI_CC=cc"
 OMNI_MPI_FC="MPI_FC=ftn"
 
