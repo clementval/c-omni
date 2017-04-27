15bc1bd9aa058aa65b650eeee0804da3e919ef1b
Valentin Clement <clementval@gmail.com>
2017-04-27 15:45:59 +0200

    Specify mpi wrappers

--- clementon2.gnu.sh
+++ clementon2.gnu.sh
@@ -1,3 +1,8 @@
+# Basic compiler used for OMNI Compiler compilation
 OMNI_FC="gfortan"
 OMNI_CC="gcc-5"
 OMNI_CXX="gcc-5"
+
+# MPI wrapper used by OMNI compiler
+OMNI_MPI_CC="MPI_CC=mpicc"
+OMNI_MPI_FC="MPI_FC=mpif90"
