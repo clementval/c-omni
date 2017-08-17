776696a3b81409af91a766f63cfb4d27d0446469
Valentin Clement <clementval@gmail.com>
2017-08-17 10:19:20 +0200

    Update mpi include path

--- compiler/kesch.gnu.sh
+++ compiler/kesch.gnu.sh
@@ -9,4 +9,4 @@ export OMNI_CXX="g++"
 export OMNI_MPI_CC="MPI_CC=mpicc"
 export OMNI_MPI_FC="MPI_FC=mpif90"
 
-export INCLUDE_MPI="/usr/local/include/"
+export INCLUDE_MPI="/apps/escha/UES/RH6.7/easybuild/software/MVAPICH2/2.2a-GCC-4.9.3-binutils-2.25/include/"
