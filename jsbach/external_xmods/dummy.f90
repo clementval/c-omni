aafce9310f6b7e65370c4ae8f0e8f82c88e99fa6
Valentin Clement <clementval@gmail.com>
2018-03-22 13:00:16 +0100

    Delete bad files

--- jsbach/external_xmods/dummy.f90
+++
@@ -1,6 +0,0 @@
-module mod1
-  use iso_c_binding_small
-  integer(C_INT64_T) :: test
-  integer(C_INT64_T), parameter :: mask1 = 2_C_INT64_T_K
-  integer(C_INT64_T), parameter :: mask2 = 2_C_INT64_T
-end module mod1
--- jsbach/external_xmods/iso_c_binding_small.f90
+++
@@ -1,6 +0,0 @@
-module iso_c_binding_small
-  implicit none
-  integer, parameter :: C_INT64_T_K = 8
-  integer (KIND=4), parameter :: C_INT64_T = 8
-end module
-
