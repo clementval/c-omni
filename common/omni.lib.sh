4844c08b2118aae7e991390098f95eb92280636f
Valentin Clement <clementval@gmail.com>
2018-05-28 17:40:52 +0200

    Add new error_type

--- common/omni.lib.sh
+++ common/omni.lib.sh
@@ -135,6 +135,7 @@ function omni::check_error_log {
   error_type18=$(grep -c "right hand side expression is not a pointee" "${__parse_log}")
   error_type19=$(grep -c "can't determine type of constant expression" "${__parse_log}")
   error_type20=$(grep -c "compiler error" "${PARSING_OUTPUT}")
+  error_type21=$(grep -c "is not found in module" "${PARSING_OUTPUT}")
 
   if [[ ${error_type1} -ne 0 ]] || [[ ${error_type2} -ne 0 ]]   \
     || [[ ${error_type3} -ne 0 ]] || [[ ${error_type4} -ne 0 ]]   \
@@ -145,7 +146,8 @@ function omni::check_error_log {
     || [[ ${error_type13} -ne 0 ]] || [[ ${error_type14} -ne 0 ]] \
     || [[ ${error_type15} -ne 0 ]] || [[ ${error_type16} -ne 0 ]] \
     || [[ ${error_type17} -ne 0 ]] || [[ ${error_type18} -ne 0 ]] \
-    || [[ ${error_type19} -ne 0 ]] || [[ ${error_type20} -ne 0 ]]; then
+    || [[ ${error_type19} -ne 0 ]] || [[ ${error_type20} -ne 0 ]] \
+    || [[ ${error_type21} -ne 0 ]]; then
     echo "ERROR: Some errors have been detected"
     echo "         ${error_type1} errors/warnings [only function/subroutine statement are allowed in contains top level] found in log"
     echo "         ${error_type2} errors/warnings [declaration among executables] found in log"
@@ -167,6 +169,7 @@ function omni::check_error_log {
     echo "         ${error_type18} errors/warnings [right hand side expression is not a pointee] found in log"
     echo "         ${error_type19} errors/warnings [can't determine type of constant expression] found in log"
     echo "         ${error_type20} errors/warnings [compiler error] found in log"
+    echo "         ${error_type21} errors/warnings [is not found in module] found in log"
     echo "       More information in the file: ${__parse_log}"
     echo "====================================="
   fi
